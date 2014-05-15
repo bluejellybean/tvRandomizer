f739b471df20d5fe4193ec4c93546155d5ff7552
Alex Barkell <alexbarkell@gmail.com>
2014-05-15 02:31:39 -0400

    Cleaned up/removed old unneeded functions

--- main.py
+++ main.py
@@ -5,125 +5,6 @@ import sys
 import random
 import subprocess
 
-#########################
-#gets file names from specified dir
-#x = os.listdir("\\AWESOMEPCV2\Users\Public\AWESOME DOPE MOVIE FOLDER")
-#x =  os.getcwd()
-#print x
-
-
-def getFileStuff():
-    x = os.getcwd()
-    y = x + "/star trek/ranMovie.txt"
-    try:
-        f = open(y, 'r')          ####a###########create text file, change path. 
-        currentAccounts = f.read().splitlines()
-        f.close()
-        return currentAccounts
-
-    except:
-        fileErrorMessage = ''.join(['[',str(currentTime()),']',' Unable to open currentAccounts.txt, terminating program...' ])
-        print (fileErrorMessage)
-
-        sys.exit()    
-
-
-def makeSickDataforWriteFileStuff(showTitle, showEpisodes):
-    data = {}
-    swag=['blah', 'notblah']
-    data[showTitle] = showEpisodes
-    data["key"] = "value"
-   # data[showTitle] = "fuckSpok.bl"
-       
-    print data
-
-
-
-#name = "star trek"
-#shows = ["spoklove.avi", "cobainletu-s.jpg"]
-#makeSickDataforWriteFileStuff(name, shows)
-def writeFileStuff():
-    x = os.getcwd()
-    y = x + "/star trek/ranMovie.txt"
-    print y
-    try:
-        f = open(y, 'w')
-        for x in range(0, 10):
-            string = str("blah" + str(x))
-            newLine = ''.join([string,'\n'])
-            f.write(newLine)
-          #  
-
-    except:
-        print('some file shit broke')
-        sys.exit()
-    f.close()
-
-    
-#print os.path.abspath(os.curdir)
-#os.chdir(os.curdir + "/Star Trek/")
-#print os.path.abspath(os.curdir)
-
-
-#play an single file, hardcoded currently
-def playFile(filePath):
-#   p = subprocess.Popen([filePath])
-    p = subprocess.Popen(["C:/Program Files (x86)/VideoLAN/VLC/vlc.exe","C:\\\Users\Alex\Desktop\Anchorman2.mp4"])
-
-def showAvailable(showName):
-    showAvailable = False
-    tvShowNamesFolder = (os.listdir(os.getcwd()))
-
-    for showTitleFolder in tvShowNamesFolder:
-        if showTitleFolder.lower() == showName.lower():
-            showAvailable = True
-
-    return showAvailable
-
-def main():
-    tv = TvShow()
-    
-    tv.getUserInput()
-    
-    
-
-    handleUserInput(showNameInput)
-
-
-    tvShowNamesFolder = (os.listdir(os.getcwd()))
-
-    for showTitleFolder in tvShowNamesFolder:
-        if showTitleFolder.lower() == showNameInput.lower():
-                print (showTitleFolder.lower()+ " is an available show!")
-
-                #os.chdir(os.curdir + "/" + myInput)
-                
-                pathTousersShowInputFolder = os.path.abspath(os.curdir)
-                print pathTousersShowInputFolder
-                writeFileStuff()
-                usersShowInputFolder = (os.listdir(pathTousersShowInputFolder))
-
-    #walking scrip area######
-                for root, dirs, files in os.walk(pathTousersShowInputFolder):
-                    for name in dirs:
-                        try:
-                            pathToSeason = (pathTousersShowInputFolder+ "/" +name)
-                          #  print pathToSeason
-                            x = os.listdir(pathToSeason)
-                            for y in x:
-                                if y != "Thumbs.db":
-                                    fullPath = (pathToSeason + "/" + y)
-                                   # print y
-                                    #print fullPath
-                                
-                            #os.rmdir(os.path.join(root, name))
-                        except WindowsError:
-                            print 'Skipping', os.path.join(root, name)
-
-#doStuff()
-
-
-#main()
 #better name for this? it will end up being the main class holding
 #data for dirs, tv names, user args and such
 class TvShows:
@@ -132,7 +13,7 @@ class TvShows:
         self.showTitle = ""
         self.userArgument = ""
         self.showDirectory = "//AWESOMEPCV2/Users/Public/AWESOME DOPE MOVIE FOLDER/Dope tv shows/"
-        self.saveDirectory = "//AWESOMEPCV2/Users/Public/AWESOME DOPE MOVIE FOLDER/Dope tv shows/__randomSaves/"
+        self.saveDirectory = self.showDirectory + "__randomSaves/"
         self.saveFile = ""
         self.fullEpisodeList = []
     
@@ -153,9 +34,18 @@ class TvShows:
         f = open(self.saveFile, 'w')
         for episode in self.fullEpisodeList:
             f.write(episode+'\n')
-           # newLine = ''.join([string,'\n'])
-            #f.write(newLine)
         f.close()
+
+    def __openShowListFromFile(self):
+        try:
+            f = open(self.saveFile, 'r')         ####a###########create text file, change path. 
+            self.fullEpisodeList = f.read().splitlines()
+            f.close()
+            print self.fullEpisodeList
+
+        except:
+            print('unable to read from file')
+            sys.exit()
 
     def __randomizeShowOrder(self):
         self.__getFilesInTvShow()
@@ -179,9 +69,15 @@ class TvShows:
         userArg = self.userInput[dashIndex:]
         self.userArgument = userArg
 
+    #TODO: change this to work with next episode
+    def playSingleEpsiode(self):
+        p = subprocess.Popen(["C:/Program Files (x86)/VideoLAN/VLC/vlc.exe","C:\\\Users\Alex\Desktop\Anchorman2.mp4", '--play-and-exit', '--fullscreen'], shell=True)
+
+
     def startShow(self):
         if self.userArgument == "":
-            print('play.vlc')
+            print('read file, play newest file, appendpop to end')
+            self.__openShowListFromFile()
         else:
             self.handleUserArguents()
 
@@ -205,25 +101,18 @@ class TvShows:
         self.saveFile = self.saveDirectory + self.showTitle + ".txt"
       
 
-    def getUserInput(self):
+    def getAndSetUserInput(self):
         #self.userInput = raw_input("Enter movie title: ")
         #temp hardcoded because sublimes compiler can't handle input
-        self.userInput = "staR trek -randomize"
+        self.userInput = "Star trek"
         self.__setUserInputs()
 
-#x = TvShows("test", "-randomize")
 
 
 tv = TvShows()
-tv.getUserInput()
+tv.getAndSetUserInput()
 
 tv.startShow()
-
-
-
-#print(os.listdir("\\AWESOMEPCV2\Users\Public\AWESOME DOPE MOVIE FOLDER\Dope tv shows"))
-print(os.listdir("//AWESOMEPCV2/Users/Public/AWESOME DOPE MOVIE FOLDER/Dope tv shows"))
-
 
 
 
